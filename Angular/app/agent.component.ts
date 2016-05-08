af891c3a9f7c067dff8cff8f02ba010b13bb71bc
Andre Smith <AndreS@SPATIAL.local>
2016-05-08 17:21:14 +0200

    no message

--- Angular/app/agent.component.ts
+++ Angular/app/agent.component.ts
@@ -1,5 +1,6 @@
 ///<reference path="./../typings/breeze.d.ts" />
 import {Component, Input} from 'angular2/core';
+import {FormBuilder, ControlGroup , Control, Validators} from 'angular2/common'
 import {Router, RouteParams} from 'angular2/router';
 import {Agent} from './DataClasses/Agent';
 import {AgentStatus} from './DataClasses/AgentStatus';
@@ -15,11 +16,21 @@ import {Observable} from 'rxjs/Observable';
 export class AgentComponent {
     //Agent:  Observable<Agent>;
     Agent:  Agent;
+    IsEditable: Boolean = false;
+    form: ControlGroup;
     
-    constructor(private _AgentService: AgentService, private _router: Router, private _routeParams: RouteParams) {
+    constructor(private _AgentService: AgentService, private _router: Router, private _routeParams: RouteParams, formBuilder: FormBuilder) {
         
         // Load the agent ... 
         //this.Agent = this._AgentService.getAgentByID(this._routeParams.get('id'));
         this._AgentService.getAgentByID(this._routeParams.get('id')).subscribe((agent:Agent)=>this.Agent = agent);
+        
+        this.form = formBuilder.group({
+            codename: ['', Validators.required]
+        })
+    }
+    
+    editAgent(){
+        this.IsEditable =!this.IsEditable;
     }
 }
